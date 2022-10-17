1ed6616548f4b9c5d7962886ae1a7c5275e0264c
wbkch <waleedbinkhalidch@gmail.com>
2022-10-17 11:07:31 +0500

    updated

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -31,12 +31,6 @@ const Resume = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="resume">Resume</Link></h2>
-          {/* <div className="link-container">
-            {sections.map((sec) => (
-              <h4 key={sec}>
-                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>))}
-          </div> */}
 
         </div>
       </header>
